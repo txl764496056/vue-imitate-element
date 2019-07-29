30bde9d8dc5be259026c70fdade6b62fe00dd596
txl <764496056@qq.com>
2019-07-29 18:33:34 +0800

    清除

--- src/components/ImInput.vue
+++ src/components/ImInput.vue
@@ -3,9 +3,11 @@
         <input  
         v-bind="$attrs"
         :value="value"
-         @input="handleInput"
+         @focus="inputFocus"
+         @blur="inputBlur"
+         @change="inputChange"
         />
-        <i class="clear"></i>
+        <i class="clear-btn" v-show="isShowClear"></i>
     </div>
 </template>
 
@@ -24,9 +26,20 @@
                 default:false
             }
         },
+        data(){
+            return {
+                isShowClear:false
+            }
+        },
         methods:{
-            handleInput(evt){
+            inputChange(evt){
                 this.$emit('change',evt.target.value);
+            },
+            inputFocus(){
+                this.isShowClear = true;
+            },
+            inputBlur(){
+                this.isShowClear = false;
             }
         }
     }
@@ -46,19 +59,6 @@ input::-webkit-input-placeholder{
         }
         &:disabled{
             background-color:middle-gray(0.05);border-color:$color-ee;
-        }
-    }
-    .clear{
-        $size:16px;
-        display:inline-block;width:$size;height:$size;position:absolute;right:5px;top:50%;margin-top:-($size/2);border:1px solid $color-bb;border-radius:50%;
-        &::after,&::before{
-            height:1px;background-color:$color-bb;width:80%;content:"";display:inline-block;position:absolute;transform-origin:center center;bottom:50%;left:2px;
-        }
-        &::after{
-            transform:rotate(45deg);
-        }
-        &::before{
-            transform:rotate(-45deg);
         }
     }
 }
