7c4637e2b892d5021e90f395969afa5bd6f2a8d6
txl <764496056@qq.com>
2019-08-13 15:25:35 +0800

    787978

--- src/components/ImSelect/ImSelect.vue
+++ src/components/ImSelect/ImSelect.vue
@@ -88,5 +88,23 @@ import { setTimeout } from 'timers';
             top:none;
         }
     }
+    .option-list{
+        $size:8px;
+        position:absolute;top:100%;left:0;right:0;margin-top:$size;z-index:99;background-color:#fff;
+        box-shadow:0 0 5px middle-gray(0.1);
+        .select-scroll{
+            position:relative;z-index:5;border-radius:5px;
+        }
+        &::before{
+            position:absolute;width:$size;height:$size;display:inline-block;content:"";left:20px;top:-($size / 1.5) + 1;
+            border: 1px solid #ddd {
+                bottom:none;
+                right:none;
+            };
+            z-index:10;
+            transform:rotate(45deg);
+            background-image:linear-gradient(135deg,#fff 50%,transparent 50%);background-size:100% 100%;
+        }
+    }
 }
 </style>
