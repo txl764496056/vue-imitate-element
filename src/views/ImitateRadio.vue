4a1e90bad0a2d0217f3aec434c0ca8a20bf4f505
txl <764496056@qq.com>
2020-06-04 18:42:34 +0800

    单选数据优化

--- src/views/ImitateRadio.vue
+++ src/views/ImitateRadio.vue
@@ -2,7 +2,6 @@
     <div>
          <!-- 选中/禁用 -->
         <div class="title">选中/禁用</div>
-
         <div class="group-unit">
           <im-radio  
             v-for="item in im_radio"
@@ -16,7 +15,7 @@
 
         <!-- 单选组 -->
         <div class="title">单选组</div>
-
+        <!-- (1) -->
         <div class="title2" data-index="1">禁用属性在 组(父im-radio-group)标签上</div>
         <div class="group-unit">
           <p class="tips">这组radio有2个父组件</p>
@@ -24,58 +23,58 @@
             v-model="im_radio_group_selected1"
             name="'im-group1'">
                 <im-radio 
-                v-for="item in im_radio_group"
+                v-for="item in im_radio_group1"
                 :key="item.label"
                 :label="item.label">选项{{item.label}}</im-radio>
             </im-radio-group>
             <p class="remark">显示选中选项的标签值：<span>{{im_radio_group_selected1}}</span></p>
         </div>
-
-        <!-- 禁用属性在组标签上 -->
+        <!-- (2)  禁用属性在组标签上 -->
         <div class="title2" data-index="2">禁用属性在 组(父im-radio-group)标签上</div>
         <div class="group-unit">
           <p class="tips">这组radio有2个父组件</p>
             <im-radio-group
-            v-model="im_radio_group_selected1"
+            v-model="im_radio_group_selected2"
             :disabled="true"
-            :name="'im-group1'">
+            :name="'im-group2'">
               <!-- 嵌套一个父组件,检测子组件在获取父的时候是否有影响 -->
               <empty-test> 
                 <im-radio 
-                v-for="item in im_radio_group"
+                v-for="item in im_radio_group2"
                 :key="item.label"
                 :label="item.label">选项{{item.label}}</im-radio>
               </empty-test>
             </im-radio-group>
-            <p class="remark">显示选中选项的标签值：<span>{{im_radio_group_selected1}}</span></p>
+            <p class="remark">显示选中选项的标签值：<span>{{im_radio_group_selected2}}</span></p>
         </div>
-
+        <!-- 未禁用 -->
         <div class="group-unit">
+            <p class="tips">父组件未禁用</p>
             <im-radio-group
-            v-model="im_radio_group_selected2"
-            :disabled="true"
-            :name="'im-group2'">
+            v-model="im_radio_group_selected3"
+            :disabled="false"
+            :name="'im-group3'">
                 <im-radio 
-                v-for="item in im_radio_group"
+                v-for="item in im_radio_group3"
                 :key="item.label"
                 :label="item.label">选项{{item.label}}</im-radio>
             </im-radio-group>
-            <p class="remark">显示选中选项的标签值：<span>{{im_radio_group_selected2}}</span></p>
+            <p class="remark">显示选中选项的标签值：<span>{{im_radio_group_selected3}}</span></p>
         </div>
 
-        <!-- 禁用属性在每个子标签上 -->
+        <!-- (3) 禁用属性在每个子标签上 -->
         <div class="title2" data-index="3">禁用属性在 子标签上</div>
         <div class="group-unit">
           <im-radio-group
-          v-model="im_radio_group_selected3"
-          :name="'im-group3'">
+          v-model="im_radio_group_selected4"
+          :name="'im-group4'">
               <im-radio 
-              v-for="item in im_radio_group"
+              v-for="item in im_radio_group4"
               :key="item.label"
               :disabled="item.disabled"
               :label="item.label">选项{{item.label}}</im-radio>
           </im-radio-group>
-          <p class="remark">显示选中选项的标签值：<span>{{im_radio_group_selected3}}</span></p>
+          <p class="remark">显示选中选项的标签值：<span>{{im_radio_group_selected4}}</span></p>
         </div>
         
     </div>
@@ -95,24 +94,38 @@ import EmptyTest from '@/components/EmptyTest.vue'
         },
         data(){
             return {
-                // 单选框
+                // 选中/禁用
                 im_radio_selected:'4',
                 im_radio:[
                     {label:'4',disabled:true},
                     {label:'5',disabled:false},
                     {label:'6',disabled:true},
                     {label:'7',disabled:false}
-
                 ],
+                // 单选组-1
                 im_radio_group_selected1:1,
-                im_radio_group_selected2:2,
-                im_radio_group_selected3:3,
-                im_radio_group:[
+                im_radio_group1:[
                     {label:1,disabled:true},
                     {label:2,disabled:false},
                     {label:3,disabled:false}
                 ],
+                // 单选组-2-1
+                im_radio_group_selected2:"1",
+                im_radio_group2:[
+                    {label:1,disabled:true},
+                    {label:2,disabled:false},
+                    {label:3,disabled:false}
+                ],
+                // 单选组-2-2
+                im_radio_group_selected3:3,
                 im_radio_group3:[
+                    {label:1,disabled:true},
+                    {label:2,disabled:false},
+                    {label:3,disabled:false}
+                ],
+                // 单选组-3
+                im_radio_group_selected4:"2",
+                im_radio_group4:[
                     {label:1,disabled:true},
                     {label:2,disabled:false},
                     {label:3,disabled:false}
